dcd8aab8ff95b44292c0af8513f41d1ff9c7df13
Vijay S <vijay.s@yavar.ai>
2026-05-21 07:03:48 +0530

    query params & route param

--- Bookly/main.py
+++ Bookly/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello World"}
+
+
+@app.get('/greet')
+async def greet_name(name: Optional[str] = "User", age: int = 0) -> dict:
+    return {"message": f"Hello {name}! You are {age}."}
